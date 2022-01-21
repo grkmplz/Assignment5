6262740ed62c96326dc6e9c00afeb69dc3305354
grkmplz <94660631+grkmplz@users.noreply.github.com>
2022-01-21 12:57:38 +0100

    Add files via upload

---
+++ Assignment5-versione/Assignment5-versione/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace assignment5_trial_trial2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Hogetdatetimee = DateTime.UtcNow.ToString("yyyy-MM-dd");
+            string dosyaadiinikoy = Hogetdatetimee + ".txt";
+
+            string yoluciz = @"C:\Users\gorke\Desktop\";
+            string kombinet = System.IO.Path.Combine(yoluciz, dosyaadiinikoy);
+
+            List<string> allvalwewant = new List<string>();
+            Console.WriteLine("Please write the text : ");
+            string valueue;
+
+          
+            bool cingalo = true;                                     // 2.Yçntem ve hocanın istediği yöntem
+            do
+            {
+                valueue = Console.ReadLine();
+                if (valueue == "start")
+                {
+                    do
+                    {
+                        valueue = Console.ReadLine();
+                        if (valueue != "stop")
+                        {
+                            allvalwewant.Add(valueue);
+                        }
+                    } while (valueue != "stop");
+
+                    using (System.IO.StreamWriter dosca = new System.IO.StreamWriter(kombinet))
+                    {
+                        dosca.WriteLine("Captains's log \nStardate " + Hogetdatetimee + "\n");
+                        foreach (var allwewant in allvalwewant)
+                        {
+                            dosca.WriteLine(allwewant);
+                        }
+                        dosca.WriteLine("\n" + "Jean-Luc Picard");
+                    }
+
+                    if (valueue == "stop")
+                    {
+                        cingalo = false;
+                    }
+                }
+
+            } while (cingalo != false);
+
+
+        }
+    }
+}
